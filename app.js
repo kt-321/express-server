2b1d93aac09bb0c80286f5eb424c321e4b3813b6
kt-321 <kt.shzk247@gmail.com>
2023-02-17 18:14:35 +0900

    change port num

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var express = require("express");
 var app = express();
 
-/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-var server = app.listen(3000, function(){
+/* 2. listen()メソッドを実行して3001番ポートで待ち受け。*/
+var server = app.listen(3001, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
@@ -15,12 +15,12 @@ var photoList = [
         id: "001",
         name: "photo001.jpg",
         type: "jpg",
-        dataUrl: "http://localhost:3000/data/photo001.jpg"
+        dataUrl: "http://localhost:3001/data/photo001.jpg"
     },{
         id: "002",
         name: "photo002.jpg",
         type: "jpg",
-        dataUrl: "http://localhost:3000/data/photo002.jpg"
+        dataUrl: "http://localhost:3001/data/photo002.jpg"
     }
 ]
 
